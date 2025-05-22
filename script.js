1c89e58c6dba3914d493b64e90b2354d966fe266
Julie Dedieu <julie.dedieu@agence-cerberus.fr>
2025-05-22 14:36:17 +0200

    loadQuotesData function refactoring

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ $(document).ready(function () {
   const carousel = $("#carouselExampleControls");
   const loader = $(".loader");
 
-  const loadData = (data) => {
-    $(".carousel-item:nth-child(1) img").attr("src", data[0].pic_url);
-    $(".carousel-item:nth-child(1) .quote-text p").text(data[0].text);
-    $(".carousel-item:nth-child(1) h4").text(data[0].name);
-    $(".carousel-item:nth-child(1) span").text(data[0].title)
-    $(".carousel-item:nth-child(2) img").attr("src", data[1].pic_url);
-    $(".carousel-item:nth-child(2) .quote-text p").text(data[1].text);
-    $(".carousel-item:nth-child(2) h4").text(data[1].name);
-    $(".carousel-item:nth-child(2) span").text(data[1].title);
+  const loadQuotesData = (data) => {
+    for (let i = 1; i <= data.length; i++) {
+    $(`.carousel-item:nth-child(${i}) img`).attr("src", data[i -1].pic_url);
+    $(`.carousel-item:nth-child(${i}) .quote-text p`).text(data[i -1].text);
+    $(`.carousel-item:nth-child(${i}) h4`).text(data[i -1].name);
+    $(`.carousel-item:nth-child(${i}) span`).text(data[i -1].title);
+    }
   }
 
   const loadQuotes = () => {
@@ -22,12 +20,12 @@ $(document).ready(function () {
       dataType: "json",
       error: function(xhr, status, error) {
         console.log(xhr.status);
-        console.loaf(error);
+        console.log(error);
         loader.removeClass("show");
       },
       success: function (data) {
         if (data !== null) {
-          loadData(data);
+          loadQuotesData(data);
           setTimeout(() => {
             loader.removeClass("show");
             carousel.removeClass("d-none");
